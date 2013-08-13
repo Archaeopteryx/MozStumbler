8b65996966d3851431473edc51d3e2ebf948d3cf
Doug Turner <doug.turner@gmail.com>
2013-08-12 20:01:09 -0700

    More lint

--- src/org/mozilla/mozstumbler/DateTimeUtils.java
+++ src/org/mozilla/mozstumbler/DateTimeUtils.java
@@ -6,15 +6,15 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+@SuppressLint("SimpleDateFormat")
 final class DateTimeUtils {
-  private static final DateFormat mISO8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'", Locale.US);
+    private static final DateFormat mISO8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
 
     static final long MILLISECONDS_PER_DAY = 86400000; // milliseconds/day
 
     private DateTimeUtils() {
     }
 
-    @SuppressLint("SimpleDateFormat")
     static String formatDate(Date date) {
         return mISO8601Format.format(date);
     }
